934d4069fb493a95d3e09e2b4e3dfda3ce4631a8
Godson <imiebo.godson@gmail.com>
2022-02-06 19:44:05 +0100

    major additions

--- src/sections/aboutMe/AboutMe.jsx
+++ src/sections/aboutMe/AboutMe.jsx
@@ -5,7 +5,20 @@ const AboutMe = () => {
     return (
         <div id='aboutMe' className='aboutMe'>
             <div className='aboutMe-wrapper container'>
-                <div className='projects-toptext'>
+                <div className='top-of-section-texts-wrapper'>
+                    <div className='top-of-section-texts'>
+                        <div className='top-of-section-texts-items'>
+                            <div className='left-text'>
+                                <p>N</p>
+                                <p className='smaller-left-text'>o</p>
+                                <p className='left-text-number'>2</p>
+                            </div>
+                            <p className='right-text'>What I am about?</p>
+                        </div>
+                        
+                    </div>
+                </div>
+                {/* <div className='projects-toptext'>
                     <div className='topmost-smalltext-wrapper'>
                         <p>ABOUT ME</p><span>-</span>
                     </div>
@@ -13,13 +26,22 @@ const AboutMe = () => {
                         <p className='topmost-bigtext'>What I Am About?</p>
                         <GiBandana className='folder-icon'/>
                     </div>
-                </div>
+                </div> */}
                 <div className='aboutMe-main-content-wrapper'>
                     <div className='aboutMe-text-main-wrapper'>
-                        <p className='aboutMe-text-main'>Passionate Front-end Developer, a <span>learner</span> and a <span>visually-oriented</span> engineer adept at delivering <span>amazing user experiences</span>.</p>
+                        <p className='aboutMe-text-main'>Passionate Front-end Developer, a <span>fast learner</span> and a <span>visually-oriented</span> engineer adept at delivering <span>amazing user experiences</span>.</p>
                     </div>
                     <div className='aboutMe-stacklist-wrapper'>
-                        <p>STACKS</p>
+                        <p><span>STACK</span> Adept with technologies such as; JavaScript, React, NextJS, CSS, SCSS, CSS-in-JS, NodeJS, ExpressJS, MongoDB, PostgreSQL...<br/><a href='https://github.com/ImieboGodson' target='_blank' rel='noreferrer' className='aboutMe-links'>SEE MY GITHUB</a></p>
+                        {/* <a href='https://github.com/ImieboGodson' target='_blank' rel='noreferrer' className='aboutMe-links'>Checkout My Github</a> */}
+                    </div>
+                    <div className='aboutMe-hobbies-wrapper'>
+                        <p><span>HOBBIES</span> I spend my time; Visiting new places, Hanging out with friends, Listening to podcasts about the state tech in Africa, Trying to learn new languages, writing about random stuff,  Watching Football or Basketball, Playing Video games, Watching movies...<br/><a href='https://dev.to/imiebogodson' target='_blank' rel='noreferrer' className='aboutMe-links'>VIEW MY WRITINGS</a></p>
+                        {/* <a href='https://github.com/ImieboGodson' target='_blank' rel='noreferrer' className='aboutMe-links'>Checkout My Github</a> */}
+                    </div>
+                    <div className='aboutMe-quote-wrapper'>
+                        <p><span>Words of Wisdom</span><br/> "The function of man is to live, not to exist.<br/> I shall not waste my days trying to prolong them. I shall use my time."</p>
+                        {/* <a href='https://github.com/ImieboGodson' target='_blank' rel='noreferrer' className='aboutMe-links'>Checkout My Github</a> */}
                     </div>
                 </div>
                     
--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -10,7 +10,20 @@ const Projects = () => {
             <Header />
             <div className='projects'>
                 <div className='projects-wrapper container'>
-                    <div className='projects-toptext'>
+                    <div className='top-of-section-texts-wrapper'>
+                        <div className='top-of-section-texts'>
+                            <div className='top-of-section-texts-items'>
+                                <div className='left-text'>
+                                    <p>S</p>
+                                    <p className='smaller-left-text'>o</p>
+                                    <p className='left-text-number'>3</p>
+                                </div>
+                                <p className='right-text'>Stuff I have Built</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    {/* <div className='projects-toptext'>
                         <div className='topmost-smalltext-wrapper'>
                             <p>FEATURED PROJECTS</p><span>-</span>
                         </div>
@@ -18,19 +31,22 @@ const Projects = () => {
                             <p className='topmost-bigtext'>Stuff I've Worked On</p>
                             <AiTwotoneFolderOpen className='folder-icon'/>
                         </div>
+                    </div> */}
+                    <div className='projectlist-wrapper'>
+                        <ProjectsList />
                     </div>
-                    <ProjectsList />
                     <div className='bottom-wrapper'>
                         <div className='bottom-smalltext-wrapper'>
                             <p className='bottom-smalltext'>There's more</p>
                         </div>
-                        <a className='viewallproject-btn-wrapper' href='https://github.com/ImieboGodson' target='_blank' rel='noreferrer'>
-                            <div className='viewallproject-btn'>
-                                <p className='btn-text'>View all projects</p>
-                                <BiRightArrow className='viewallproject-btn-icon'/>
-                            </div>
-                        </a>
-                        
+                        <div className='viewallproject-btn-wrapper'>
+                            <a className='viewallproject-anchorTag' href='https://github.com/ImieboGodson' target='_blank' rel='noreferrer'>
+                                <div className='viewallproject-btn'>
+                                    <p className='btn-text'>View all projects</p>
+                                    <BiRightArrow className='viewallproject-btn-icon'/>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
