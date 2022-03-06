8c6a050cc968f9beb9b3466ab9104c34f44f112e
Godson <imiebo.godson@gmail.com>
2022-03-06 21:30:20 +0100

    preloader functionality and styling

--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -1,10 +1,25 @@
 import './preloader.scss'
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const Preloader = () => {
   return (
     <div className='preloader'>
         <div className='preloader-image-wrapper'>
-            <img src='assets/tractor-preloader.gif' alt='preloader' className='preloader-image'/>
+            <img src='assets/progressbar-1.gif' alt='preloader' className='preloader-image'/>
+            {/* <ProgressBar 
+              completed={34}
+              bgColor="#000000"
+              height="100%"
+              width="100%"
+              borderRadius=""
+              labelAlignment="outside"
+              baseBgColor="#ffffff"
+              labelColor="#000000"
+              labelSize="35px"
+              transitionDuration="15s"
+              animateOnRender
+              maxCompleted={100}
+            /> */}
         </div>
         <div className='preloader-name-wrapper'>
           <p className='preloader-name'>Godson Imiebo</p>
--- src/pages/layoutWrapper/LayoutWrapper.jsx
+++ src/pages/layoutWrapper/LayoutWrapper.jsx
@@ -5,17 +5,40 @@ import Contact from '../../sections/contact/Contact';
 import AboutMe from '../../sections/aboutMe/AboutMe';
 import Footer from '../../components/footer/Footer';
 import Preloader from '../../components/preloader/Preloader';
+import { useEffect, useState } from 'react';
 
 const LayoutWrapper = () => {
 
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 3500)
+    }, [])
+    
+
     return (
         <div className='layoutWrapper'>
-            <Hero />
-            <AboutMe />
-            <Projects />
-            <Contact />
-            <Footer />
-            <Preloader />
+
+            {
+                !loading 
+                ?
+                (
+                    <>
+                        <Hero />
+                        <AboutMe />
+                        <Projects />
+                        <Contact />
+                        <Footer />
+                    </> 
+                )
+                :
+                (
+                    <Preloader />
+                )
+            }
         </div>
     )
 }
