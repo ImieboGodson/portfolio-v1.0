b38b19edb24f3be12688560fe7508eb67d2f05db
Godson <imiebo.godson@gmail.com>
2022-04-03 22:17:06 +0100

    preloader fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,62 @@ import {
 } from "react-router-dom";
 import Project from './pages/project/Project';
 import Header from './components/header/Header';
+import Preloader from './components/preloader/Preloader';
+import { useEffect, useState } from 'react';
+import { ProjectsArray } from './data';
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [progress, setProgress] = useState(0)
+
+
+
+  useEffect(() => {
+
+      setLoading(true);
+
+      let progressPercentage = 0;
+      
+      for(let i = 0; i <= 1000; i++) {
+          progressPercentage = Math.floor((i / 1000) * 100);
+
+          console.log('Progress: ', progressPercentage);
+
+          setProgress(progressPercentage);
+      }
+
+      console.log('Final Progress: ', progressPercentage);
+
+      // setTimeout(() => {
+      //     setLoading(false)
+      // }, 5000)
+  }, []);
+
+
+
   return (
     <div className="app">
-      <Header />
-      <Routes>
-        <Route path='/' element={<LayoutWrapper />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/projects/:name' element={<Project />}/>
-      </Routes>
+
+              {
+                !loading 
+                ?
+                (
+                  <>
+                    <Header />
+                    <Routes>
+                      <Route path='/' element={<LayoutWrapper />}/>
+                      <Route path='/about' element={<About />}/>
+                      <Route path='/projects/:name' element={<Project />}/>
+                    </Routes>
+                  </>
+                )
+                :
+                (
+                    <Preloader progress={progress}/>
+                )
+              }
+      
     </div>
   );
 }
--- src/components/preloader/Preloader.jsx
+++ src/components/preloader/Preloader.jsx
@@ -1,22 +1,36 @@
 import './preloader.scss'
+import { preLoaderAnim } from '../../animations/index'
+import { useEffect } from 'react';
 // import ProgressBar from "@ramonak/react-progress-bar";
 
-const Preloader = ({ progress }) => {
+const Preloader = () => {
+
+  useEffect(() => {
+    preLoaderAnim();
+  }, []);
+
   return (
-    <div className='preloader'>
-        <div className='progress-bar-wrapper'>
-          <div className='progress-bar'>
-            <div className='progress-bar-content' style={{width: `${progress}%`, height: '100%', backgroundColor: '#fff', transition: 'ease-in-out 5s'}}></div>
-          </div>
-          <p className='progress-bar-label'>{`${progress}%`}</p>
-        </div>
-        <div className='preloader-name-wrapper'>
-          <p className='preloader-name'>Godson Imiebo</p>
-        </div>
-        <div className='preloader-subtext-wrapper'>
-          <p className='preloader-subtext'>Frontend Developer in Lagos, Nigeria</p>
-        </div>
+    <div className="preloader">
+      <div className="texts-container">
+        <span>Developer,</span>
+        <span>Curator,</span>
+        <span>Vibes.</span>
+      </div>
     </div>
+    // <div className='preloader'>
+    //     <div className='progress-bar-wrapper'>
+    //       <div className='progress-bar'>
+    //         <div className='progress-bar-content' style={{width: `${progress}%`, height: '100%', backgroundColor: '#fff', transition: 'ease-in-out 10s'}}></div>
+    //       </div>
+    //       <p className='progress-bar-label'>{`${progress}%`}</p>
+    //     </div>
+    //     <div className='preloader-name-wrapper'>
+    //       <p className='preloader-name'>Godson Imiebo</p>
+    //     </div>
+    //     <div className='preloader-subtext-wrapper'>
+    //       <p className='preloader-subtext'>Frontend Developer in Lagos, Nigeria</p>
+    //     </div>
+    // </div>
   )
 }
 
